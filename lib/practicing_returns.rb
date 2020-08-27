c3b3978b7d418d8334b9c64f7660c35d60c3f7bb
Trey Jennings <tjennings343@gmail.com>
2020-08-27 14:36:02 +0000

    Automatically backed up by Learn

--- lib/practicing_returns.rb
+++ lib/practicing_returns.rb
@@ -10,3 +10,13 @@ end
 
 
 hello(["Tim", "Tom", "Jim"]) { |name| "Hi, #{name}" }
+
+def hello(array)
+  i = 0
+  collection = []
+  while i < array.length
+    collection << yield(array[i])
+    i += 1
+  end
+  collection
+end
